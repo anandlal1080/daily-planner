cace0dc7d5dbb96bd06c33faf6e3e87f34928528
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2020-11-11 20:58:15 -0700

    Updated Text box to show data

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,40 @@
+let currentDay = $("#currentDay").text(luxon.DateTime.local().toFormat("ff"));
 
-for(let hour = 9; hour < 18; hour++) {
-    $(".container").append(createTimeSlot(hour));
+
+
+for (let hour = 9; hour < 18; hour++) {
+  $(".container").append(createTimeSlot(hour));
 }
 function createTimeSlot(hour) {
   const $timeSlot = $("<div>")
-  .attr("id", `hour-${hour}`)
-  .attr("value", hour)
-  .attr("class", "row time-block");
-  const $timeLabel = $("<div>").attr("class", "col-md-1 hour");
+    .attr("id", `hour-${hour}`)
+    .attr("value", hour)
+    .attr("class", "row time-block");
+  const $timeLabel = $("<div>").attr("class", "col-md-1 hour").addClass("currentHour");
   if (hour > 12) {
     $timeLabel.text(`${hour - 12} PM`);
   } else if (hour === 12) {
     $timeLabel.text(`${hour} PM`);
   } else {
-      $timeLabel.text(`${hour} AM`);
+    $timeLabel.text(`${hour} AM`);
   }
-  const $textArea = $("<textarea>")
-  .attr("class", "col-md-10 description");
+  const $textArea = $("<textarea>").attr("class", "col-md-10 description").attr("text", hour);
   const $saveBtn = $("<button>")
-  .attr("class", "btn saveBtn col-md-1")
-  .append($("<i>").attr("class", "fas fa-save"));
+    .attr("class", "btn saveBtn col-md-1")
+    .append($("<i>").attr("class", "fas fa-save"));
   $timeSlot.append($timeLabel, $textArea, $saveBtn);
-  
-  let currentTime = luxon.DateTime.local().toFormat("H");
-  $(".container").each(function(block){
-    console.log($timeSlot.attr("value"));
-    if($timeSlot.attr("value") < currentTime) {
-        $textArea.addClass("past");
-        } else if ($timeSlot.attr("value") == currentTime){
-          $textArea.addClass("present");
-        } else{
-          $textArea.addClass("future");
-        }
-});
+
+  let currentTime = parseInt(luxon.DateTime.local().toFormat("H"));
+
+  $(".container").each(function (block) {
+   
+    if ($timeSlot.attr("value") < currentTime) {
+      $textArea.addClass("past");
+    } else if ($timeSlot.attr("value") == currentTime) {
+      $textArea.addClass("present");
+    } else {
+      $textArea.addClass("future");
+    }
+  });
   return $timeSlot;
 }
---
+++ assets/textinput.js
@@ -0,0 +1,23 @@
+let $textFromUser = $("textarea");
+let $hourOfCalendy = $(".col-md-1 hour currentHour");
+const $saveBtn = $(".saveBtn");
+let plansForDay = localStorage.getItem("plansForDay");
+if (plansForDay){
+    plansForDay = JSON.parse(plansForDay);
+} else {
+    plansForDay = [];
+}
+$saveBtn.on("click", function(e){
+    plansForDay.push({
+        currentHour: $(this).parent().attr('id').split('-')[1],
+        currentText: $(this).siblings('textarea').val() // <-- 
+    });
+    console.log(plansForDay);
+    localStorage.setItem("plansForDay", JSON.stringify(plansForDay));
+   
+})
+
+
+plansForDay.forEach(function(plan) {
+    $("div[value='" + plan.currentHour + "']").children('textarea').text(plan.currentText);
+  })
