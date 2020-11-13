0b07505f87d137f3d8e766c366d30012ea3be402
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2020-11-12 19:55:41 -0700

    Added comments

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
+// Added variable to display the Date and Time in the page header.
 let currentDay = $("#currentDay").text(luxon.DateTime.local().toFormat("ff"));
 
+// For loop to create each of the timeslot hour using the createTimeSlot function
 for (let hour = 9; hour < 18; hour++) {
   $(".container").append(createTimeSlot(hour));
 }
+
+// This function creates the time slots for each hour form 9am-6pm. It also converts the 24HR times to PM from 1PM onwards.
 function createTimeSlot(hour) {
   const $timeSlot = $("<div>")
     .attr("id", `hour-${hour}`)
@@ -16,14 +20,17 @@ function createTimeSlot(hour) {
   } else {
     $timeLabel.text(`${hour} AM`);
   }
+
+  // Defined the variable for the text area and save button to be rendrered.
   const $textArea = $("<textarea>").attr("class", "col-md-10 description").attr("text", hour);
   const $saveBtn = $("<button>")
     .attr("class", "btn saveBtn col-md-1")
     .append($("<i>").attr("class", "fas fa-save"));
   $timeSlot.append($timeLabel, $textArea, $saveBtn);
 
+  // declared a variable to get teh current time. This will be used in the next function to color the text area.
   let currentTime = parseInt(luxon.DateTime.local().toFormat("H"));
-
+// This function sets the color of the text area based on the current time. Grey for past, Red for current hour and green for future hour.
   $(".container").each(function (block) {
    
     if ($timeSlot.attr("value") < currentTime) {
--- assets/textinput.js
+++ assets/textinput.js
@@ -1,28 +1,35 @@
+// This variable targets the text area for each hour.
 let $textFromUser = $("textarea");
+// This variable gets the hour from the specific hour row we are referencing.
 let $hourOfCalendy = $(".col-md-1 hour currentHour");
+// This variable is to get teh save button for each row.
 const $saveBtn = $(".saveBtn");
+// This variable gets data from local storage and is used in the upcoming if/else statement.
 let plansForDay = localStorage.getItem("plansForDay");
+// This if/else statement parses data from local storage if any exists. Else it returns an empty array.
 if (plansForDay){
     plansForDay = JSON.parse(plansForDay);
 } else {
     plansForDay = [];
 }
+
+// This function will save the data from the text area to the corresponding hour into local storage.
 $saveBtn.on("click", function(e){
     plansForDay.push({
         currentHour: $(this).parent().attr('id').split('-')[1],
         currentText: $(this).siblings('textarea').val() // <-- 
     });
-    console.log(plansForDay);
+    
     localStorage.setItem("plansForDay", JSON.stringify(plansForDay));
    
 })
 
-
+// This forEach statement & function will take the data from plansForDay line 11 and displays it on the corresponding hour row when the page is loaded/refreshes.
 plansForDay.forEach(function(plan) {
     $("div[value='" + plan.currentHour + "']").children('textarea').text(plan.currentText);
   })
 
-
+// This function clears local storage when the user clicks on the "Clear Planner" button.
   $("#clear").on("click", function(){
     window.localStorage.clear();
     location.reload();
